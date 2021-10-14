2f59c637a60c37f47fab93e38e01e18649e4a3a3
Thayná Cristina <thaynacristinadsf@gmail.com>
2021-10-14 13:59:28 -0300

    Upload arquivo .php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <title>Básico html</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body class='tela'>
+    Aqui vai o código HTML que fará seu site aparecer.
+  </body>
+</html>
